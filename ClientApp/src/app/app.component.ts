1858a53797b49ffe280392f1fbbe3c72376021a0
emandere <emandere@gmail.com>
2018-09-23 05:39:39 -0400

    first commit

---
+++ ClientApp/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { BehaviorSubject} from "rxjs/BehaviorSubject";
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'material-project';
+  private stateString:BehaviorSubject<string> = new BehaviorSubject("Start yyyy");
+
+  getState():Observable<string>{
+    return this.stateString.asObservable();
+  }
+
+  changeState()
+  {
+     let state:string = this.stateString.value;
+     this.stateString.next(state+"n");
+  }
+
+}
---
+++ ClientApp/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import{ MatButtonModule, MatListModule } from "@angular/material";
+import {MatMenuModule} from '@angular/material/menu';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from "@angular/material/icon";
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {BehaviorSubject} from "rxjs/BehaviorSubject";
+import {MatInputModule} from '@angular/material/input';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatListModule,
+    MatIconModule,
+    MatSidenavModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
